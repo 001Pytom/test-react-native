7d835a39c2d8e3a680c8b7634f973af5579cb16e
faatihat <tomfaah@gmail.com>
2025-08-13 11:30:14 +0100

    vid1&2

---
+++ app/index.jsx
@@ -0,0 +1,50 @@
+import { StyleSheet, Text, View, Image } from "react-native";
+import React from "react";
+import myImg from "../assets/img/YUSUF.jpg";
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      {/* use uri if you are using online image */}
+      {/* <Image source={{ uri: "https://reactnative.dev/img/tiny_logo.png" }} /> */}
+
+      <Image source={myImg} style={styles.img} />
+
+      <Text style={styles.title}>The Number one</Text>
+      <Text>Reading list App</Text>
+
+      <View style={styles.box}>
+        <Text>This is a box</Text>
+      </View>
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, //default stuff here is in column direction
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  img: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    marginBottom: 20,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  box: {
+    marginTop: 20,
+    backgroundColor: "#eee",
+    padding: 20,
+    borderRadius: 5,
+    boxShadow:
+      "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+  },
+});
